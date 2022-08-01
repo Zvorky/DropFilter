0220db68e863431b775316f5cbb902a3ab5da3a2
Zvorky <enzzavorski@edu.univali.br>
2022-08-01 19:07:10 -0300

    DropFilter class, last code, not working properly

--- DropFilter.py
+++ DropFilter.py
@@ -9,174 +9,176 @@
 
 import os, sys, time, json
 from gi.repository import Notify, GLib
-import DropFilter_lib
 
+class DropFilter:
+    icon = "/usr/share/icons/hicolor/scalable/apps/DropFilter_icon.svg"
+    home = os.getenv('HOME')
+    configDir = home + "/.dropfilter"
+    config = "/config.json"
+    log = ""
 
-configDir = os.getenv('HOME') # temporarily home
+    def __init__(self, config = "/config.json"):
+        Notify.init("DropFilter")
+        self.ntf = Notify.Notification.new("DropFilter", "", DropFilter.icon)
 
-#   Default Config
-SleepTime = 20; #Seconds
+        os.system("clear")
+        print("=| DropFilter |=\nv0.6")
 
-File      = {"Any": [""],
-            "Code": [".cpp",".h",".py",".sh"],
-            "PDF": [".pdf"],
-            "Video": [".mp4",".mkv",".webm",".mov"],
-            "Audio": [".mp3",".ogg",".wav"],
-            "Image": [".gif",".pgn",".jpeg","jpg"],
-            "Vector": [".svg", ".eps", ".ai", ".cdr"],
-            "Compressed": [".zip",".rar",".tar",".gz",".xz",".tgz",".jar",".deb",".qdz",".run",".exe",".rpm"],
-            "Document": [".odt",".odp",".ods",".odf",".doc",".docx",".ppt",".pptx"]}
+        #   Config
+        print("\nChecking configuration files...\n\n")
 
-Directory = {"DropFilter": configDir + "/.dropfilter",
-            "Dropbox": configDir + "/Dropbox",
-            "Desktop": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DESKTOP),
-            "Downloads": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DOWNLOAD)}
+        #   Home .dropfilter dir
+        if(not os.path.exists(DropFilter.configDir)):
+            log = DropFilter.configDir + " not found\n"
+            self.ntf.update("DropFilter", DropFilter.configDir + " not found, creating.", DropFilter.icon)
+            self.ntf.show()
+            os.mkdir(DropFilter.configDir)
+            log = log + DropFilter.configDir + " created.\n"
+        else:
+            log = DropFilter.configDir + " found.\n"
+        print(log)
 
-Filter =    [[["Desktop"],"PDF", "DropFilter"],
-            [["Downloads"],"Code", "DropFilter"]]
+        #   Temp directory
+        if(not os.path.exists(DropFilter.configDir + "/tmp")):
+            os.mkdir(DropFilter.configDir + "/tmp")
+            log = log + DropFilter.configDir + "/tmp created.\n"
+            print(DropFilter.configDir + "/tmp created.")
+        else:
+            log = log + DropFilter.configDir + "/tmp found.\n"
+            print(DropFilter.configDir + "/tmp found.")
+        
+        #   Log File        !!! needs to be closed o.o -----------------------------------------------------------------------------------
+        logfile = DropFilter.configDir + "/tmp/" + time.asctime() + ".txt"
+        os.system("touch \"" + logfile + "\"")
+        logfile = open(logfile, "a")
+        logfile.write("=| DropFilter Log - " + time.asctime() + " |=\nv0.6\n\n" + log)
+        log = ""
 
-#   DropFilter
-icon = "/usr/share/icons/hicolor/scalable/apps/DropFilter_icon.svg"
-configDir = configDir + "/.dropfilter"
-config = "/config.json"
-log = ""
+        #   Main config file
+        try:
+            with open(DropFilter.configDir + "/config.json", "xt") as config:
+                configjson = {  "SleepTime": 20,
 
+                                "File": {   "Any": [""],
+                                            "Code": [".cpp",".h",".py",".sh"],
+                                            "PDF": [".pdf"],
+                                            "Video": [".mp4",".mkv",".webm",".mov"],
+                                            "Audio": [".mp3",".ogg",".wav"],
+                                            "Image": [".gif",".pgn",".jpeg","jpg"],
+                                            "Vector": [".svg", ".eps", ".ai", ".cdr"],
+                                            "Compressed": [".zip",".rar",".tar",".gz",".xz",".tgz",".jar",".deb",".qdz",".run",".exe",".rpm"],
+                                            "Document": [".odt",".odp",".ods",".odf",".doc",".docx",".ppt",".pptx"] },
+                                
+                                "Directory": {  "DropFilter": DropFilter.home + "/.dropfilter",
+                                                "Dropbox": DropFilter.home + "/Dropbox",
+                                                "Desktop": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DESKTOP),
+                                                "Downloads": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DOWNLOAD)},
+                                
+                                "Filter": [ [["Desktop"],"PDF", "DropFilter"],
+                                            [["Downloads"],"Code", "DropFilter"]]  }
+                
+                config.write(json.dumps(configjson))
+                config.close()
 
+                log = DropFilter.configDir + "/config.json created.\n"
+                print(log)
+                self.ntf.update("DropFilter", log, DropFilter.icon)
+                self.ntf.show()
+                log = log + "\nDefault Config: " + json.dumps(configjson)
 
-def verify(config = "/config.json"):
-    global SleepTime, File, Directory, Filter, icon, configDir, log
+        except FileExistsError:
+            log = "config.json found.\n"
 
-    #   Reload Log
-    logfile = open(log, "a")
+        logfile.write(time.asctime() + " |: " + log)
+        logfile.close()
+        DropFilter.log = logfile.name
+
+        self.load(config)
+
+        self.ntf.update("DropFilter", "v0.6 - " + config + " All ready, " + os.getenv('USER') + "!", DropFilter.icon)
+        self.ntf.show()
+
 
     #   Load Config
-    try:
-        with open(configDir + config) as config:
-            configjson = json.load(config)
+    def load(self, config = "/config.json"):
+        #   Reload Log
+        logfile = open(log, "a")
+        
+        try:
+            with open(DropFilter.configDir + config) as config:
+                configjson = json.load(config)
 
-            if(SleepTime != configjson["SleepTime"]):
-                SleepTime = configjson["SleepTime"]
+                if(self.SleepTime != configjson["SleepTime"]):
+                    self.SleepTime = configjson["SleepTime"]
 
-            if(File != configjson["File"]):
-                File      = configjson["File"]
+                if(self.File != configjson["File"]):
+                    self.File      = configjson["File"]
 
-            if(Directory != configjson["Directory"]):
-                Directory = configjson["Directory"]
+                if(self.Directory != configjson["Directory"]):
+                    self.Directory = configjson["Directory"]
 
-            if(Filter != configjson["Filter"]):
-                Filter    = configjson["Filter"]
-                log = "\nConfig File: " + json.dumps(configjson)
-                logfile.write(time.asctime() + " |: " + log)
+                if(self.Filter != configjson["Filter"]):
+                    self.Filter    = configjson["Filter"]
+                    log = "\nConfig File: " + json.dumps(configjson)
+                    logfile.write(time.asctime() + " |: " + log)
 
-                print(log)
-                ntf.update("Configuration Loaded", "SleepTime: " + str(SleepTime) + "s.", icon)
-                ntf.show()
-                log = ""
-    finally:
-        config.close()
+                    print(log)
+                    self.ntf.update("Configuration Loaded", "SleepTime: " + str(self.SleepTime) + "s.", DropFilter.icon)
+                    self.ntf.show()
+                    log = ""
+        
+        finally:
+            config.close()
+    
 
-    for filter in Filter:
-        for source in filter[0]:
-            try:
-                source = Directory[source]
+    def verify(self, config = "/config.json"):
+        # global SleepTime, File, Directory, Filter, icon, configDir, log
 
-            finally:
-                if(os.path.exists(source)):
-                    for file in os.listdir(source):
-                        command = "mv \""
+        #   Reload Log
+        logfile = open(log, "a")
 
-                            for end in File[filter[1]]:
+        for filter in self.Filter:
+            for source in filter[0]:
+                try:
+                    source = self.Directory[source]
+
+                finally:
+                    if(os.path.exists(source)):
+                        for file in os.listdir(source):
+                            for end in self.File[filter[1]]:
                                 if(file.endswith(end)):
                                     #   Notify and move the file to the Filtered directory
                                     print(file + " found at \"" + dir + "\", moving it...")
                                     try:
-                                        filter[2] = Directory[filter[2]]
+                                        filter[2] = self.Directory[filter[2]]
 
                                     finally:
                                         if(not os.path.exists(filter[2])):
                                             mkdir(filter[2])
-                                        os.system("mv -n \"" + DropDir[dir] + "/" + file + "\" \"" + filter[2] + "/" + file + "\"")
+                                        os.system("mv -n \"" + self.Directory[dir] + "/" + file + "\" \"" + filter[2] + "/" + file + "\"")
 
                                         log = file + " moved to \"" + filter[2] + "\".\n"
                                         logfile.write(time.asctime() + " |: " + log)
 
                                         print(log)
-                                        ntf.update(file, log, icon)
-                                        ntf.show()
+                                        self.ntf.update(file, log, DropFilter.icon)
+                                        self.ntf.show()
                                         log = ""
 
-                else:
-                    log = source + " don't exists."
-                    logfile.write(time.asctime() + " |: " + log)
-                    print(log)
-                    log = ""
+                    else:
+                        log = source + " don't exists."
+                        logfile.write(time.asctime() + " |: " + log)
+                        print(log)
+                        log = ""
 
-    logfile.close()
-    logfile = logfile.name
-    time.sleep(SleepTime)
-    return config
+        logfile.close()
+        logfile = logfile.name
+        time.sleep(self.SleepTime)
+        return config
 
 
-
-if __name__ == '__main__':
-    Notify.init("DropFilter")
-    ntf = Notify.Notification.new("DropFilter", "", icon)
-
-    os.system("clear")
-    print("=| DropFilter |=\nv0.6")
-
-    #   Config
-    print("\nChecking configuration files...\n\n")
-
-    #   Home .dropfilter dir
-    if(not os.path.exists(configDir)):
-        log = configDir + " not found\n"
-        ntf.update("DropFilter", configDir + " not found, creating.", icon)
-        ntf.show()
-        os.mkdir(configDir)
-        log = log + configDir + " created.\n"
-    else:
-        log = configDir + " found.\n"
-    print(log)
-
-    #   Temp directory
-    if(not os.path.exists(configDir + "/tmp")):
-        os.mkdir(configDir + "/tmp")
-        log = log + configDir + "/tmp created.\n"
-        print(configDir + "/tmp created.")
-    else:
-        log = log + configDir + "/tmp found.\n"
-        print(configDir + "/tmp found.")
-
-    #   Log File        !!! needs to be closed o.o
-    logfile = configDir + "/tmp/" + time.asctime() + ".txt"
-    os.system("touch \"" + logfile + "\"")
-    logfile = open(logfile, "a")
-    logfile.write("=| DropDir Log - " + time.asctime() + " |=\nv0.6\n\n" + log)
-    log = ""
-
-    #   Main config file
-    try:
-        with open(configDir + "/config.json", "xt") as config:
-            configjson = {"SleepTime": SleepTime, "File": File, "DropDir": DropDir, "FilterDir": FilterDir}
-            config.write(json.dumps(configjson))
-            config.close()
-
-            log = configDir + "/config.json created.\n"
-            print(log)
-            ntf.update("DropFilter", log, icon)
-            ntf.show()
-            log = log + "\nDefault Config: " + json.dumps(configjson)
-
-    except FileExistsError:
-        log = "config.json found.\n"
-
-    logfile.write(time.asctime() + " |: " + log)
-    logfile.close()
-    log = logfile.name
-
-    ntf.update("DropFilter", "v0.6 - All ready, " + os.getenv('USER') + "!", icon)
-    ntf.show()
-
-    while True:
-        config = verify(config)
+#   Recursive make directory
+def mkdir(dir: str):
+    if(not os.path.exists(dir[0:dir.rfind('/')])):
+        mkdir(dir[0:dir.rfind('/')])
+    os.system("mkdir \"" + dir + "\"")
