48720745746dac875ff0043523c3bd62c88839e5
Enzo Zavorski Delevatti <46092863+Zvorky@users.noreply.github.com>
2022-08-04 18:49:43 -0300

    Log Class created
    Created Log Class, not implemented in DropFilter yet.

--- DropFilter.py
+++ DropFilter.py
@@ -15,7 +15,7 @@ class DropFilter:
     home = os.getenv('HOME')
     configDir = home + "/.dropfilter"
     config = "/config.json"
-    log = ""
+    log = Log('DropFilter', 0.6)
 
     def __init__(self, config = "/config.json"):
         Notify.init("DropFilter")
@@ -46,8 +46,8 @@ class DropFilter:
         else:
             log = log + DropFilter.configDir + "/tmp found.\n"
             print(DropFilter.configDir + "/tmp found.")
-        
-        #   Log File        !!! needs to be closed o.o -----------------------------------------------------------------------------------
+
+        #   Log File, not implemented Log Class yet
         logfile = DropFilter.configDir + "/tmp/" + time.asctime() + ".txt"
         os.system("touch \"" + logfile + "\"")
         logfile = open(logfile, "a")
@@ -68,15 +68,15 @@ class DropFilter:
                                             "Vector": [".svg", ".eps", ".ai", ".cdr"],
                                             "Compressed": [".zip",".rar",".tar",".gz",".xz",".tgz",".jar",".deb",".qdz",".run",".exe",".rpm"],
                                             "Document": [".odt",".odp",".ods",".odf",".doc",".docx",".ppt",".pptx"] },
-                                
+
                                 "Directory": {  "DropFilter": DropFilter.home + "/.dropfilter",
                                                 "Dropbox": DropFilter.home + "/Dropbox",
                                                 "Desktop": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DESKTOP),
                                                 "Downloads": GLib.get_user_special_dir(GLib.USER_DIRECTORY_DOWNLOAD)},
-                                
+
                                 "Filter": [ [["Desktop"],"PDF", "DropFilter"],
                                             [["Downloads"],"Code", "DropFilter"]]  }
-                
+
                 config.write(json.dumps(configjson))
                 config.close()
 
@@ -100,10 +100,10 @@ class DropFilter:
 
 
     #   Load Config
-    def load(self, config = "/config.json"):
+    def load(self, config):
         #   Reload Log
         logfile = open(log, "a")
-        
+
         try:
             with open(DropFilter.configDir + config) as config:
                 configjson = json.load(config)
@@ -126,13 +126,16 @@ class DropFilter:
                     self.ntf.update("Configuration Loaded", "SleepTime: " + str(self.SleepTime) + "s.", DropFilter.icon)
                     self.ntf.show()
                     log = ""
-        
+
         finally:
             config.close()
-    
 
-    def verify(self, config = "/config.json"):
+
+    def verify(self, config = ""):
         # global SleepTime, File, Directory, Filter, icon, configDir, log
+
+        if(config = ""):
+            config = self.config
 
         #   Reload Log
         logfile = open(log, "a")
@@ -175,6 +178,100 @@ class DropFilter:
         logfile = logfile.name
         time.sleep(self.SleepTime)
         return config
+
+
+
+class Log:
+    def __init__(self, path: str, title: str, version, console = True, notify = True, icon = DropFilter.icon):
+        self.title = title
+        # self.subtitle = ''
+        self.version = version
+        self.console = console
+        self.notify = notify
+        self.file = path + time.asctime() + ".txt"
+
+        text = "=| " + title + " Log - " + time.asctime() + " |=\nv" + version + "\n\n" + self.file + "\nNotify: " + notify
+        self << text    #   Create, Append and Close file
+
+        if(notify):
+            Notify.init(title)
+            self.notify = Notify.Notification.new(title, "Initializing v" + version + "...", icon)
+            self.notify.show()
+
+        if(console):
+            print(text)
+
+    #   Append to log file
+    def __lshift__(self, text: str):
+        # try:
+        #     with open(self.file, "a") as log:
+        #         log.write(text + '\n')
+        #
+        # except FileNotFoundError:
+        mkdir(self.path)
+        os.system("touch \"" + self.file + "\"")
+
+        with open(self.file, "a") as log:
+            log.write(text + '\n')
+
+        # finally:
+        log.close()
+
+    #   Logs neutral message
+    def log(self, message: str, newNotify = False):
+        text = "~ | " + time.asctime() + " |: " + message
+
+        self << text
+
+        if(self.console):
+            print(text)
+
+        if(self.notify):
+            if(newNotify):
+                self.notify.new(self.title, message, self.icon)
+            else:
+                self.notify.update(self.title, message, self.icon)
+            self.notify.show()
+
+    #   Logs Informative message
+    def info(self, message: str, subtitle: str, newNotify = True):
+        self << "¡ " + subtitle + " | " + time.asctime() + " |: " + message
+
+        if(self.console):
+            print("\033[0;1;30;47m ¡ " + subtitle + "\033[2;7;37m \033[0m| " + time.asctime() + " |: " + message)
+
+        if(self.notify):
+            if(newNotify):
+                self.notify.new(subtitle, message, self.icon)
+            else:
+                self.notify.update(subtitle, message, self.icon)
+
+    #   Logs Warning message
+    def warn(self, message: str, subtitle: str, newNotify = True):
+        self << "/!\\ " + subtitle + " | " + time.asctime() + " |: " + message
+
+        if(self.console):
+            print("\033[0;5;1;30;43m /!\\ " + subtitle + "\033[2;7;33m \033[0m| " + time.asctime() + " |: " + message)
+
+        if(self.notify):
+            if(newNotify):
+                self.notify.new("⚠️ " + subtitle, message, self.icon)
+            else:
+                self.notify.update("⚠️ " + subtitle, message, self.icon)
+
+    #   Logs Failure message
+    def error(self, message: str, subtitle = '', newNotify = True):
+        self << "ERROR " + subtitle + " | " + time.asctime() + " |: " + message
+
+        if(self.console):
+            print("\033[0;1;30;41m ERROR " + subtitle + "\033[2;7;31m \033[0m| " + time.asctime() + " |: " + message)
+
+        if(self.notify):
+            if(newNotify):
+                self.notify.new("ERROR " + subtitle, message, self.icon)
+            else:
+                self.notify.update("ERROR " + subtitle, message, self.icon)
+
 
 
 #   Recursive make directory
